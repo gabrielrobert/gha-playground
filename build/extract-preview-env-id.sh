a3225aea4347f269625cc1ec48afc91d90ca965e
Gabriel Robert <g.robert092@gmail.com>
2025-02-13 13:18:34 -0500

    try stuff

--- build/extract-preview-env-id.sh
+++ build/extract-preview-env-id.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -e
 
-if [ -n "$GITHUB_REF" ]; then
-  # If GITHUB_REF ends with -preview-env, extract the PR number
-  if [[ $GITHUB_REF == *"-preview-env" ]]; then
-    # If the GITHUB_REF starts with a number, extract the PR number
-    PR_NUMBER=$(echo $GITHUB_REF | sed -n 's/.*\/\([0-9]*\)-preview-env/\1/p')
-  fi
-  echo "PR_NUMBER=$PR_NUMBER" >>$GITHUB_ENV
-else
-  echo "NO GITHUB_REF"
-fi
+GITHUB_REF="refs/pull/1/merge"
+
+PR_NUMBER=$(echo $GITHUB_REF | sed -n 's/.*\/\([0-9]*\)-preview-env/\1/p')
+echo "PR_NUMBER: $PR_NUMBER"
+
+ISSUE_NUMBER=$(echo $GITHUB_REF | sed -n 's/.*\/\([0-9]*\)-preview-env/\1/p')
+echo "ISSUE_NUMBER: $ISSUE_NUMBER"
+
+## Get the github.event.pull_request.number
+PR_NUMBER=$(jq --raw-output .pull_request.number "$GITHUB_EVENT_PATH")
